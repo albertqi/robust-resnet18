32f49c7c7cc1c3f8cbd29187b120df4b1e5db879
Albert Qi <albertqi03@gmail.com>
2024-04-12 05:17:43 -0400

    Overhaul main.py

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
-import matplotlib.pyplot as plt
-import sys, torch
+import torch
 from common import DATA_DIR, TRANSFORMS
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 from torchvision.datasets import ImageNet
 from torchvision.models import resnet34, ResNet34_Weights
 from torchvision.transforms import v2
 from tqdm import tqdm
 
 
-def visualize(transform):
-    """Visualize the transformation."""
-
-    # Load the data.
-    val_dataset = ImageNet(DATA_DIR, split="val", transform=transform)
-
-    # Visualize the data.
-    figure = plt.figure(figsize=(6, 6))
-    cols, rows = 2, 2
-    for i in range(1, cols * rows + 1):
-        sample_idx = torch.randint(len(val_dataset), size=(1,)).item()
-        img, label = val_dataset[sample_idx]
-        figure.add_subplot(rows, cols, i)
-        plt.title(f"Class {label}")
-        plt.axis("off")
-        plt.imshow(img)
-    plt.show()
+NUM_EPOCHS = 50
+SUBSET_SIZE = 0.05
 
 
-def train(transform):
-    """Train the model with the given transformation."""
+class RandomTransform:
+    """Randomly apply a transformation."""
 
+    def __init__(self, preprocessing, transforms):
+        self.preprocessing = preprocessing
+        self.transforms = transforms
+
+    def __call__(self, img):
+        transform = self.transforms[
+            torch.randint(len(self.transforms), size=(1,)).item()
+        ]
+        composed = (
+            v2.Compose([self.preprocessing, transform])
+            if transform
+            else self.preprocessing
+        )
+        return composed(img)
+
+
+def main():
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
     # Define the preprocessing pipeline.
     weights = ResNet34_Weights.DEFAULT
     preprocessing = weights.transforms()
 
-    # Load the data.
-    transform = v2.Compose([preprocessing, transform]) if transform else preprocessing
+    # Load the training data.
+    transform = RandomTransform(preprocessing, TRANSFORMS)
     train_dataset = ImageNet(DATA_DIR, split="train", transform=transform)
+    subset_size = int(SUBSET_SIZE * len(train_dataset))
+    subset_indices = torch.randperm(len(train_dataset))[:subset_size].tolist()
+    sampler = SubsetRandomSampler(subset_indices)
     train_dataloader = DataLoader(
-        train_dataset, batch_size=32, num_workers=8, shuffle=True
+        train_dataset, batch_size=32, num_workers=8, sampler=sampler
+    )
+
+    # Load the validation data.
+    val_dataset = ImageNet(DATA_DIR, split="val", transform=transform)
+    subset_size = int(SUBSET_SIZE * len(val_dataset))
+    subset_indices = torch.randperm(len(val_dataset))[:subset_size].tolist()
+    sampler = SubsetRandomSampler(subset_indices)
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=32, num_workers=8, sampler=sampler
     )
 
     # Define the model.
-    model = resnet34(weights=weights).to(device)
+    model = resnet34().to(device)
 
     # Define the optimizer.
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
@@ -53,81 +66,50 @@ def train(transform):
     # Define the loss function.
     loss_fn = nn.CrossEntropyLoss()
 
-    # Train the model.
-    losses, accuracies = [], []
-    model.train()
-    for X, y in tqdm(train_dataloader):
-        X, y = X.view(X.size(0), -1).to(device), y.to(device)
+    for epoch in range(NUM_EPOCHS):
+        # Train the model.
+        losses, accuracies = [], []
+        model.train()
+        for X, y in tqdm(train_dataloader):
+            X, y = X.to(device), y.to(device)
 
-        l = model(X)  # Forward pass.
-        J = loss_fn(l, y)  # Compute the loss.
-        optimizer.zero_grad()  # Zero the gradients.
-        J.backward()  # Backward pass.
-        optimizer.step()  # Update the parameters.
+            l = model(X)  # Forward pass.
+            J = loss_fn(l, y)  # Compute the loss.
+            optimizer.zero_grad()  # Zero the gradients.
+            J.backward()  # Backward pass.
+            optimizer.step()  # Update the parameters.
 
-        losses.append(J.item())
-        accuracies.append(y.eq(l.detach().argmax(dim=1)).float().mean())
+            losses.append(J.item())
+            accuracies.append(y.eq(l.detach().argmax(dim=1)).float().mean())
 
-    avg_loss = torch.tensor(losses).mean()
-    avg_accuracy = torch.tensor(accuracies).mean()
+        avg_loss = torch.tensor(losses).mean()
+        avg_accuracy = torch.tensor(accuracies).mean()
 
-    print(transform)
-    print(f"Avg. Training Loss: {avg_loss:.2f}")
-    print(f"Avg. Training Accuracy: {avg_accuracy:.2f}")
-    print()
+        print(f"Epoch {epoch + 1}/{NUM_EPOCHS}")
+        print(f"Avg. Training Loss: {avg_loss:.2f}")
+        print(f"Avg. Training Accuracy: {avg_accuracy:.2f}")
+        print()
 
+        # Evaluate the model.
+        losses, accuracies = [], []
+        model.eval()
+        for X, y in tqdm(val_dataloader):
+            X, y = X.to(device), y.to(device)
 
-def val(transform):
-    """Evaluate the model with the given transformation."""
+            with torch.no_grad():
+                l = model(X)
+            J = loss_fn(l, y)
 
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+            losses.append(J.item())
+            accuracies.append(y.eq(l.detach().argmax(dim=1)).float().mean())
 
-    # Define the preprocessing pipeline.
-    weights = ResNet34_Weights.DEFAULT
-    preprocessing = weights.transforms()
+        avg_loss = torch.tensor(losses).mean()
+        avg_accuracy = torch.tensor(accuracies).mean()
 
-    # Load the data.
-    transform = v2.Compose([preprocessing, transform]) if transform else preprocessing
-    val_dataset = ImageNet(DATA_DIR, split="val", transform=transform)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, num_workers=8)
-
-    # Define the model.
-    model = resnet34(weights=weights).to(device)
-
-    # Define the loss function.
-    loss_fn = nn.CrossEntropyLoss()
-
-    # Evaluate the model.
-    losses, accuracies = [], []
-    model.eval()
-    for X, y in tqdm(val_dataloader):
-        X, y = X.to(device), y.to(device)
-
-        with torch.no_grad():
-            l = model(X)
-        J = loss_fn(l, y)
-
-        losses.append(J.item())
-        accuracies.append(y.eq(l.detach().argmax(dim=1)).float().mean())
-
-    avg_loss = torch.tensor(losses).mean()
-    avg_accuracy = torch.tensor(accuracies).mean()
-
-    print(transform)
-    print(f"Avg. Validation Loss: {avg_loss:.2f}")
-    print(f"Avg. Validation Accuracy: {avg_accuracy:.2f}")
-    print()
-
-
-def main():
-    assert len(sys.argv) == 2
-    should_visualize = sys.argv[1] == "1"
-
-    for transform in TRANSFORMS:
-        if should_visualize:
-            visualize(transform)
-        else:
-            val(transform)
+        print(f"Epoch {epoch + 1}/{NUM_EPOCHS}")
+        print(f"Avg. Validation Loss: {avg_loss:.2f}")
+        print(f"Avg. Validation Accuracy: {avg_accuracy:.2f}")
+        print()
 
 
 if __name__ == "__main__":
